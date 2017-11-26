3b03b0396a157846202f1f5030f9a06b0b835fac
wzzxy1314 <mengma1993@163.com>
2017-11-26 21:19:53 +0800

    对对对

---
+++ src/main/java/com/how2java/demo/App.java
@@ -0,0 +1,13 @@
+package com.how2java.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "fuck you!" );
+    }
+}
